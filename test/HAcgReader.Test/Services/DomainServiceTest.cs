24c718bcd45e41885c24e33362390bdd99d44df0
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-07-09 20:43:03 +0800

    测试构造函数

--- test/HAcgReader.Test/Services/DomainServiceTest.cs
+++ test/HAcgReader.Test/Services/DomainServiceTest.cs
@@ -1,8 +1,8 @@
 ﻿using FluentAssertions;
 using HAcgReader.Services;
+using HAcgReader.Test.TestHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using Moq.Protected;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Text;
@@ -16,22 +16,15 @@ namespace HAcgReader.Test.Services;
 [ExcludeFromCodeCoverage]
 public class DomainServiceTest
 {
-    private const string SampleResponse = @"
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset=""UTF-8"">
-<title>琉璃神社★分享动漫快乐</title>
-<body>
-<div>
-<p><a href=""https://example.com"">https://example.com</a></p>
-<p>由于经常被不明人士攻击，神社会定时更换地址。</p>
-<p>大家可以收藏这个页面到书签。</p>
-<p>希望大家不要再被骗。</p>
-<p>ACG.GY</p>
-<p>ACG.公益</p>
-</div>
-";
+    /// <summary>
+    /// 测试 <see cref="DomainService.DomainService"/>
+    /// </summary>
+    [TestMethod]
+    public void TestConstructor()
+    {
+        using var service = new DomainService();
+        service.Should().NotBeNull();
+    }
 
     /// <summary>
     /// 测试 <see cref="DomainService.GetDomain"/>
@@ -46,14 +39,7 @@ public class DomainServiceTest
         };
 
         var handler = new Mock<HttpMessageHandler>();
-        handler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.Is<HttpRequestMessage>(
-                    r => r.Method == HttpMethod.Get &&
-                    r.RequestUri!.ToString().StartsWith("https://acg.gy/")),
-                ItExpr.IsAny<CancellationToken>())
-            .ReturnsAsync(httpResponse);
+        handler.SetupHttpResponse(HttpMethod.Get, new Uri("https://acg.gy"), httpResponse);
 
         using var httpClient = new HttpClient(handler.Object);
         using var service = new DomainService(httpClient);
@@ -74,18 +60,28 @@ public class DomainServiceTest
         };
 
         var handler = new Mock<HttpMessageHandler>();
-        handler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.Is<HttpRequestMessage>(
-                    r => r.Method == HttpMethod.Get &&
-                    r.RequestUri!.ToString().StartsWith("https://acg.gy/")),
-                ItExpr.IsAny<CancellationToken>())
-            .ReturnsAsync(httpResponse);
+        handler.SetupHttpResponse(HttpMethod.Get, new Uri("https://acg.gy"), httpResponse);
 
         using var httpClient = new HttpClient(handler.Object);
         using var service = new DomainService(httpClient);
 
         service.GetDomain().Should().Be(string.Empty);
     }
+
+    private const string SampleResponse = @"
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset=""UTF-8"">
+        <title>琉璃神社★分享动漫快乐</title>
+        <body>
+        <div>
+        <p><a href=""https://example.com"">https://example.com</a></p>
+        <p>由于经常被不明人士攻击，神社会定时更换地址。</p>
+        <p>大家可以收藏这个页面到书签。</p>
+        <p>希望大家不要再被骗。</p>
+        <p>ACG.GY</p>
+        <p>ACG.公益</p>
+        </div>
+    ";
 }
---
+++ test/HAcgReader.Test/TestExtensions/HttpClientExtension.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using Moq.Protected;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HAcgReader.Test.TestHelpers;
+
+/// <summary>
+/// <see cref="HttpClient"/> 的 <see cref="Mock{T}"/> 对象的辅助类
+/// </summary>
+[ExcludeFromCodeCoverage]
+public static class HttpClientExtension
+{
+    /// <summary>
+    /// 设置 HTTP 返回内容
+    /// </summary>
+    /// <param name="mock"><see cref="Mock{T}"/> 对象</param>
+    /// <param name="method">HTTP 方法</param>
+    /// <param name="uri">HTTP URI</param>
+    /// <param name="response">返回内容</param>
+    public static void SetupHttpResponse(this Mock<HttpMessageHandler> mock,
+        HttpMethod method, Uri uri, HttpResponseMessage response)
+    {
+        mock.Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.Is<HttpRequestMessage>(r => r.Method == method && r.RequestUri == uri),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(response);
+    }
+}
