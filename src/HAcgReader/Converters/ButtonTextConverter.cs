e8b0eb5cda626d9394aa6ceed7b99dc27e03067c
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-07-10 21:53:14 +0800

    在按钮不可用时显示加载信息

---
+++ src/HAcgReader/Converters/ButtonTextConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HAcgReader.Converters;
+
+public class ButtonTextConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value ? "更多" : "正在获取信息，请稍后……";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (string)value == "更多";
+    }
+}
