ef02a29e19b3b5bad3a1c8235b2b5f12779e57f9
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-07-12 15:24:08 +0800

    添加主窗口视图模型

--- src/HAcgReader/MainWindow.xaml.cs
+++ src/HAcgReader/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using HAcgReader.Services;
 using HAcgReader.ViewModels;
 using System.Diagnostics;
 using System.Globalization;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Navigation;
@@ -17,24 +16,9 @@ public partial class MainWindow : Window
 {
     #region Fields
     /// <summary>
-    /// 文章列表视图模型
+    /// 主窗口视图模型
     /// </summary>
-    public ArticleListViewModel ArticleListViewModel { get; private set; }
-
-    /// <summary>
-    /// 拉取按钮视图模型
-    /// </summary>
-    public FetchButtonViewModel FetchButtonViewModel { get; private set; }
-
-    /// <summary>
-    /// 详情页视图模型
-    /// </summary>
-    public DetailPageViewModel DetailPageViewModel { get; private set; }
-
-    /// <summary>
-    /// 滚动条视图模型
-    /// </summary>
-    public ProgressBarViewModel ProgressBarViewModel { get; private set; }
+    public MainViewModel MainViewModel { get; private set; }
     #endregion
 
     #region Constructors
@@ -54,99 +38,17 @@ public partial class MainWindow : Window
             Close();
         }
 
-        FetchButtonViewModel = new FetchButtonViewModel();
-        DetailPageViewModel = new DetailPageViewModel();
-        ProgressBarViewModel = new ProgressBarViewModel();
-        ArticleListViewModel = new ArticleListViewModel(domain);
-
-        InitializeViewModelEventHandlers();
+        MainViewModel = new MainViewModel(articleListViewModel: new ArticleListViewModel(domain),
+                                          fetchButtonViewModel: new FetchButtonViewModel(),
+                                          detailPageViewModel: new DetailPageViewModel(),
+                                          progressBarViewModel: new ProgressBarViewModel());
 
         InitializeComponent();
-        FetchButtonViewModel.Command.Execute(null);
-    }
-    #endregion
-
-    #region Methods
-    /// <summary>
-    /// 初始化视图模型事件监听器
-    /// </summary>
-    private void InitializeViewModelEventHandlers()
-    {
-        ArticleListViewModel.ArticleSelected += ArticleListViewModel_ArticleSelected;
-        ArticleListViewModel.FetchStarted += ArticleListViewModel_FetchStarted;
-        ArticleListViewModel.RssFeedFetched += ArticleListViewModel_RssFeedFetched;
-        ArticleListViewModel.PageAnalysisCompleted += ArticleListViewModel_PageAnalysisCompleted;
-        ArticleListViewModel.FetchCompleted += ArticleListViewModel_FetchCompleted;
-        FetchButtonViewModel.Clicked += FetchButtonViewModel_Clicked;
+        MainViewModel.FetchButtonViewModel.Command.Execute(null);
     }
     #endregion
 
     #region Event Handlers
-    /// <summary>
-    /// <see cref="ArticleListViewModel.ArticleSelected"/> 事件处理
-    /// </summary>
-    /// <param name="sender">事件发送者</param>
-    /// <param name="e">事件参数</param>
-    private void ArticleListViewModel_ArticleSelected(object? sender, ArticleSelectedEventArgs e)
-    {
-        DetailPageViewModel.SelectedArticle = e.SelectedArticle;
-        DetailPageViewModel.Visibility = Visibility.Visible;
-    }
-
-    /// <summary>
-    /// <see cref="ArticleListViewModel.FetchStarted"/> 事件处理
-    /// </summary>
-    /// <param name="sender">事件发送者</param>
-    /// <param name="e">事件参数</param>
-    private void ArticleListViewModel_FetchStarted(object? sender, System.EventArgs e)
-    {
-        ProgressBarViewModel.IsIndeterminate = true;
-    }
-
-    /// <summary>
-    /// <see cref="ArticleListViewModel.RssFeedFetched"/> 事件处理
-    /// </summary>
-    /// <param name="sender">事件发送者</param>
-    /// <param name="e">事件参数</param>
-    private void ArticleListViewModel_RssFeedFetched(object? sender, RssFeedFetchedEventArgs e)
-    {
-        ProgressBarViewModel.IsIndeterminate = false;
-        ProgressBarViewModel.Maximum = e.Total;
-        ProgressBarViewModel.Value = 0;
-    }
-
-    /// <summary>
-    /// <see cref="ArticleListViewModel.PageAnalysisCompleted"/> 事件处理
-    /// </summary>
-    /// <param name="sender">事件发送者</param>
-    /// <param name="e">事件参数</param>
-    private void ArticleListViewModel_PageAnalysisCompleted(object? sender, PageAnalysisCompletedEventArgs e)
-    {
-        ProgressBarViewModel.Value = e.Progress;
-    }
-
-    /// <summary>
-    /// <see cref="ArticleListViewModel.FetchCompleted"/> 事件处理
-    /// </summary>
-    /// <param name="sender">事件发送者</param>
-    /// <param name="e">事件参数</param>
-    private void ArticleListViewModel_FetchCompleted(object? sender, System.EventArgs e)
-    {
-        ProgressBarViewModel.Value = 0;
-        FetchButtonViewModel.IsEnabled = true;
-    }
-
-    /// <summary>
-    /// <see cref="FetchButtonViewModel.Clicked"/> 事件处理
-    /// </summary>
-    /// <param name="sender">事件发送者</param>
-    /// <param name="e">事件参数</param>
-    private void FetchButtonViewModel_Clicked(object? sender, System.EventArgs e)
-    {
-        FetchButtonViewModel.IsEnabled = false;
-        Task.Run(ArticleListViewModel.FetchAsync);
-    }
-
     /// <summary>
     /// 文章链接、文章评论链接点击事件处理
     /// </summary>
